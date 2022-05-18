1e456377b1c9c2db2ad6c87d18e9bdeb6b54a908
Prakash Pandey <prapandey03@gmail.com>
2022-05-18 17:52:03 +0530

    Initial commit

---
+++ hardhat_project/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x8609F60fAF6dc27f5B3A32b45E3302711eBFCA4B";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
